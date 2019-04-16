d41eab2f6b5ee5d59f69a1ac0c3eafc0e7f35ff4
Daniel Metzing <dmetzing@openwt.com>
2019-04-16 16:58:47 +0200

    Initial Commit

---
+++ HealthSyncroniser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthSyncroniser
+//
+//  Created by Daniel Metzing on 16.04.19.
+//  Copyright © 2019 Daniel Metzing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
